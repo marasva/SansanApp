c75625d879ef8850ba2f1b8cdbd83f80dc09cb6a
mariesva <mariesva@stud.ntnu.no>
2018-10-13 00:22:00 -0300

    added food view with list

--- app/src/main/java/sansanapp/aplicacionesm/usm/cl/sansanapp/MainActivity.java
+++ app/src/main/java/sansanapp/aplicacionesm/usm/cl/sansanapp/MainActivity.java
@@ -1,7 +1,10 @@
 package sansanapp.aplicacionesm.usm.cl.sansanapp;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
